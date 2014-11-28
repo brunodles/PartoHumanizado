4c1c9a32a37cba75c3de47d674357c041e5c762f
Bruno de Lima <dlimaun@gmail.com>
2014-11-28 09:38:38 -0300

    Imagens.

--- app/src/main/java/br/com/PartoHumanizado/fragment/base/TextFragment.java
+++ app/src/main/java/br/com/PartoHumanizado/fragment/base/TextFragment.java
@@ -1,6 +1,10 @@
 package br.com.PartoHumanizado.fragment.base;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.text.Html;
@@ -8,7 +12,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 import br.com.PartoHumanizado.R;
 import br.com.PartoHumanizado.util.TextAssetReader;
@@ -24,37 +32,83 @@ public class TextFragment extends BaseFragment {
     public static final String KEY_ASSET_FILENAME = "KEY_ASSET_FILENAME";
 //    public static final String KEY_TITTLE = "KEY_TITTLE";
 
-    private String tittle;
-    private String text;
+    private String tittleStr;
+    private String textStr;
     private String menuTittle;
+    private String imageFile;
 
     @InjectView(R.id.titulo)
     TextView titulo;
     @InjectView(R.id.texto)
     TextView texto;
+    @InjectView(R.id.image)
+    ImageView image;
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-//        tittle = getArguments().getString(KEY_TITTLE);
+//        tittleStr = getArguments().getString(KEY_TITTLE);
         String filename = getArguments().getString(KEY_ASSET_FILENAME);
         Log.d(TAG, "onAttach filename: " + filename);
         TextAssetReader textAssetReader = new TextAssetReader(activity, filename);
         textAssetReader.setLineBreaker("<br/>");
         textAssetReader.read();
 
-        tittle = textAssetReader.getTittle();
-        Log.d(TAG, "onAttach tittle " + tittle);
-        text = textAssetReader.getString();
+        imageFile = textAssetReader.getImage();
+        tittleStr = textAssetReader.getTittle();
+        Log.d(TAG, "onAttach tittleStr " + tittleStr);
+        textStr = textAssetReader.getString();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_text, null);
         ButterKnife.inject(this, view);
-        titulo.setText(tittle);
-        texto.setText(Html.fromHtml(text));
+        titulo.setText(tittleStr);
+        texto.setText(Html.fromHtml(textStr));
+        new LoadImageAsyncTask(getActivity(), imageFile, image).execute();
         return view;
+    }
+
+    private class LoadImageAsyncTask extends AsyncTask<Void, Void, Bitmap> {
+
+        private Context context;
+        private String imageFile;
+        private ImageView image;
+
+        private LoadImageAsyncTask(Context context, String imageFile, ImageView image) {
+            this.context = context;
+            this.imageFile = imageFile;
+            this.image = image;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            if (imageFile == null)
+                image.setVisibility(View.GONE);
+            else
+                image.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected Bitmap doInBackground(Void... params) {
+            InputStream is = null;
+            try {
+                is = context.getAssets().open(imageFile);
+                return BitmapFactory.decodeStream(is);
+            } catch (Exception e) {
+                Log.e(TAG, "doInBackground ", e);
+            }
+            cancel(true);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (bitmap != null)
+                image.setImageBitmap(bitmap);
+        }
     }
 
 
@@ -67,7 +121,7 @@ public class TextFragment extends BaseFragment {
         TextFragment fragment = new TextFragment();
         fragment.menuTittle = menuTitle;
         Bundle bundle = new Bundle();
-//        bundle.putString(KEY_TITTLE, tittle);
+//        bundle.putString(KEY_TITTLE, tittleStr);
         bundle.putString(KEY_ASSET_FILENAME, assetFileName);
         fragment.setArguments(bundle);
         return fragment;
--- app/src/main/java/br/com/PartoHumanizado/util/TextAssetReader.java
+++ app/src/main/java/br/com/PartoHumanizado/util/TextAssetReader.java
@@ -2,12 +2,18 @@ package br.com.PartoHumanizado.util;
 
 import android.content.Context;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by bruno on 27/11/14.
  */
 public class TextAssetReader extends AssetReader {
 
+    private static List<String> imageFileExtensions = Arrays.asList("jpg", "jpeg", "png");
+
     StringBuilder builder;
+    String image = null;
     String tittle = null;
     String lineBreaker = "\n";
 
@@ -18,11 +24,28 @@ public class TextAssetReader extends AssetReader {
 
     @Override
     protected void readLine(String line) {
-        if (tittle == null)
-            tittle = line;
-        else
+        if (tittle == null) {
+            if (image == null && isImage(line))
+                image = line;
+            else
+                tittle = line;
+        } else {
             builder.append(line)
                     .append(lineBreaker);
+        }
+    }
+
+    private boolean isImage(String line) {
+        if (line.startsWith("http"))
+            return true;
+        int indexOf = line.lastIndexOf(".");
+        if (indexOf == -1)
+            return false;
+        String extension = line.substring(indexOf + 1);
+        System.out.println(extension);
+        if (imageFileExtensions.contains(extension))
+            return true;
+        return false;
     }
 
     public void setLineBreaker(String lineBreaker) {
@@ -35,5 +58,9 @@ public class TextAssetReader extends AssetReader {
 
     public String getTittle() {
         return tittle;
+    }
+
+    public String getImage() {
+        return image;
     }
 }
