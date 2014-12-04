e713159b82cc295ccaf40420d4ee8dc336dff15e
Bruno de Lima <dlimaun@gmail.com>
2014-12-03 21:52:21 -0300

    Novas cores; Cantos arredondados nas imagens; Inicio do plano de parto;

---
+++ app/src/main/java/br/com/PartoHumanizado/model/PlanoDeParto.java
@@ -0,0 +1,22 @@
+package br.com.PartoHumanizado.model;
+
+/**
+ * Created by bruno on 03/12/14.
+ */
+public class PlanoDeParto {
+
+    private static enum TrabalhoDeParto{
+        acompanhante, outrosPessoasDaFamilia, lavagemIntestinal,
+        caminhar, mudarDePosicao, usoDeAgua,
+        bebidasAlimentos, agua, objetosPessoais,
+        tricotomia, infusaoIntravenosa, monitoramentoFetal,
+        ;
+    }
+
+    private static enum Parto{
+
+    }
+    private static enum PosParto{
+
+    }
+}
